991cb5383b71f9f2829d773dbd54568b29bda58e
MaximeJomain <maxime.jomain95@gmail.com>
2020-12-16 14:13:52 +0100

    ajout fonction blur

--- filters/blur.py
+++ filters/blur.py
@@ -1,4 +1,12 @@
 import cv2
 
 
-blur = cv2.GaussianBlur()
+def blur():
+
+    image = cv2.imread('image/meme.jpg', -1)
+    image = cv2.GaussianBlur(image, (51, 51), cv2.BORDER_DEFAULT)
+
+    cv2.imwrite('outputImage/blurredmeme.jpg', image)
+
+
+
--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import filters.blur as bl
+import filters.blackandwhite as fl
+
+bl.blur()
+#fl.convertimage()
+
+
 from filters.blackandwhite import convertImage
 
 convertImage()
