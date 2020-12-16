4ad2a00e2eb04ee88a7c0227179d51b2ce4c5c9e
johannaT19 <72390893+johannaT19@users.noreply.github.com>
2020-12-16 11:20:26 +0100

    supp dossier image

--- filters/blackandwhite.py
+++ filters/blackandwhite.py
@@ -1,3 +1,15 @@
 import cv2
 
-image = cv2.imread('')
+def convertimage():
+    image = cv2.imread('../image /humour.jpg')
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow('Original image', image)
+    cv2.imshow('Gray image', gray)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+
