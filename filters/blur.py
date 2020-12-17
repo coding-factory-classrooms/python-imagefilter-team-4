25f11ad4645dfdb0d41e6a9ba55efe25a007270f
MaximeJomain <maxime.jomain95@gmail.com>
2020-12-17 21:37:14 +0100

    ajout commandes CLI pour filtres avec paramètres + ajout d'une bibliothèque

--- filters/blur.py
+++ filters/blur.py
@@ -3,7 +3,6 @@ import cv2
 
 # Applique le filtre flou
 def blur_image(image):
-    blurImage = cv2.GaussianBlur(image, (55, 55), 0)
+    from configuration import cfg
+    blurImage = cv2.GaussianBlur(image, (cfg['blur_value'], cfg['blur_value']), 0)
     return blurImage
-
-
