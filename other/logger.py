e3b0693ef5a6bdd3c0fd12e6e0ff8f6111c0e452
Mancone Alexis <ale.mancone@gmail.com>
2020-12-18 10:10:45 +0100

    ajout du docstring sur le bon logger

--- other/logger.py
+++ other/logger.py
@@ -2,5 +2,10 @@ from other.data import lib
 
 
 def log(msg):
+    """
+    this function allows to write in the file imagefilter.log
+    :param msg: allows you to add an operation to the file
+    :return:displays the operation in the imagefilter.log file
+    """
     with open(lib['log_file'], 'a') as f:
         f.write(msg + '\n')
